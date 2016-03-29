9180e1c3cbce2f7efbbf696a965f6c984b6061b5
unknown <you@example.com>
2016-03-29 12:45:57 +0800

    Homework5

--- displaylist/game.js
+++ displaylist/game.js
@@ -5,19 +5,34 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 var humanContainer = new render.DisplayObjectContainer();
 var head = new render.Bitmap();
-head.source = "wander-icon.jpg";
+var trunk = new render.Bitmap();
+var left_arm = new render.Bitmap();
+var right_arm = new render.Bitmap();
+var left_leg = new render.Bitmap();
+var right_leg = new render.Bitmap();
+head.source = "head.png";
+trunk.source = "trunk.png";
+left_arm.source = "left_arm.png";
+right_arm.source = "right_arm.png";
+left_leg.source = "left_leg.png";
+right_leg.source = "right_leg.png";
 humanContainer.addChild(head);
+humanContainer.addChild(trunk);
+humanContainer.addChild(left_arm);
+humanContainer.addChild(right_arm);
+humanContainer.addChild(left_leg);
+humanContainer.addChild(right_leg);
 var renderCore = new render.RenderCore();
-renderCore.start(humanContainer, ["wander-icon.jpg"]);
+renderCore.start(humanContainer, ["head.png", "trunk.png", "left_arm.png", "right_arm.png", "left_leg.png", "right_leg.png"]);
 var HumanBody = (function (_super) {
     __extends(HumanBody, _super);
     function HumanBody() {
         _super.apply(this, arguments);
     }
     HumanBody.prototype.onTicker = function (duringTime) {
-        // this.x = 
-        // this.y = 
-        // this.rotation =
+        this.x =  
+         this.y = 
+         this.rotation =
     };
     return HumanBody;
 }(Body));
--- displaylist/game.ts
+++ displaylist/game.ts
@@ -5,11 +5,25 @@ module game {
 
 var humanContainer = new render.DisplayObjectContainer();
 var head = new render.Bitmap();
-head.source = "wander-icon.jpg";
+var trunk = new render.Bitmap();
+var left_arm = new render.Bitmap();
+var right_arm = new render.Bitmap();
+var left_leg = new render.Bitmap();
+var right_leg = new render.Bitmap();
+head.source = "head.png";
+trunk.source = "trunk.png";
+left_arm.source = "left_arm.png";
+right_arm.source = "right_arm.png";
+left_leg.source = "left_leg.png";
+right_leg.source  ="right_leg.png";
 humanContainer.addChild(head)
-
+humanContainer.addChild(trunk)
+humanContainer.addChild(left_arm)
+humanContainer.addChild(right_arm)
+humanContainer.addChild(left_leg)
+humanContainer.addChild(right_leg)
 var renderCore = new render.RenderCore();
-renderCore.start(humanContainer, ["wander-icon.jpg"]);
+renderCore.start(humanContainer, ["head.png","trunk.png","left_arm.png","right_arm.png","left_leg.png","right_leg.png"]);
 
 
 class HumanBody extends Body {
