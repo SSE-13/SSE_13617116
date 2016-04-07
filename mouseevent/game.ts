bf15dc6a32b5de5ce1aac11ad537d0f3a0096c89
unknown <you@example.com>
2016-04-07 08:16:21 +0800

    no message

--- mouseevent/game.ts
+++ mouseevent/game.ts
@@ -13,7 +13,18 @@ left_arm.source = "left_arm.png";
 right_arm.source = "right_arm.png";
 left_leg.source = "left_leg.png";
 right_leg.source  ="right_leg.png";
-
+head.x =-180;
+head.y = -60;
+trunk.x = 0;
+trunk.y=150;
+left_arm.x = -20;
+left_arm.y =160;
+right_arm.x = 20;
+right_arm.y =160;
+left_leg.x = -45;
+left_leg.y =250;
+right_leg.x = 20;
+right_leg.y = 250;
 humanContainer.addChild(human);
 
 human.addChild(head)
@@ -30,12 +41,13 @@ class HumanBody extends Body {
 
   vx:number = 5;
   vy:number  = 0;
-
+  speed = 1;
+  radius = Math.PI;
     onTicker(duringTime: number) {
 
          this.x +=this.vx*duringTime;
          this.y = this.vy*duringTime;
-         this.rotation +=Math.PI/2*duringTime;
+         this.rotation += this.radius/2*duringTime;
 
     }
 }
@@ -49,15 +61,43 @@ ticker.start([body]);
 
 var eventCore = new events.EventCore();
 eventCore.init();
-
+var HeadClicked = false;
+var LegClicked = false;
 var headHitTest = (localPoint:math.Point,displayObject:render.DisplayObject) =>{
-    alert (`点击位置为${localPoint.x},${localPoint.y}`);
+   // alert (`点击位置为${localPoint.x},${localPoint.y}`);
+    console.log(localPoint);
+    if(localPoint.x > 0  && localPoint.x <= 78 && localPoint.y > 0 && localPoint.y <= 73){
+        HeadClicked =true;
+    }   
+    if(localPoint.x > 12  && localPoint.x <= 59 && localPoint.y > 132 && localPoint.y <= 189){
+        LegClicked =true;
+    }   
     return true;
 }
 
 var headOnClick = () => {
-    alert("clicked!!");
-    //修改 HumanBody 的速度，使其反向移动
+    if(HeadClicked)
+    {        
+        if(body.vx == 0)
+        {
+            body.vx = 5 * body.speed;
+            body.radius = Math.PI * body.speed;   
+            HeadClicked = false;     
+        }else{
+            body.vx *= -1;
+            body.radius *= -1;
+            body.speed*= -1;
+            HeadClicked = false;
+        }
+    }
+    if(LegClicked)
+    {
+        body.vx = 0;
+        body.radius = 0;
+        body.rotation = 0;
+        LegClicked = false;
+    }
+   
 }
 
 eventCore.register(head,headHitTest,headOnClick);
